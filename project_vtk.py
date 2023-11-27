3d006974a0db3c1e19c45b1dcab477b12f47040f
Konstantinos Kevopoulos <113594011+kevopoulosk@users.noreply.github.com>
2023-11-27 21:26:50 +0100

    plot connections and modify shape/colour

--- project_vtk.py
+++ project_vtk.py
@@ -1,44 +1,95 @@
-import vtk
 import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+import vtk
 
-# Read data from your file
-filename = "/Users/manuelgonzaleznovo/Desktop/project_vtk/rank_0_positions.txt"
+fn_positions = "/Users/konstantinoskevopoulos/Downloads/positions/rank_0_positions.txt"
 
-# Load data with dtype=float to convert numeric values
-data = np.loadtxt(filename, skiprows=7, usecols=(1, 2, 3))
+# TODO: ADD FUNCTION THAT GETS THE POINTS PER SIMULATION
+positions_data = np.loadtxt(fn_positions, skiprows=7, usecols=(0, 1, 2, 3))
 
-# Create a vtkPoints object to store the 3D points
+# Create points for neurons
 points = vtk.vtkPoints()
+local_ids = []
 
-# Add points to the vtkPoints object
-for row in data:
-    x, y, z = row
+for row in positions_data:
+    local_id, x, y, z = row
     points.InsertNextPoint(x, y, z)
+    local_ids.append(local_id)
 
-# Create a vtkCellArray to define the structure of the data
+print(local_ids[:10])
+
+# Create cells for neurons
 verts = vtk.vtkCellArray()
-
-# Add a vertex for each point
-for i in range(len(data)):
+for i in range(len(positions_data)):
     verts.InsertNextCell(1)
     verts.InsertCellPoint(i)
 
-# Create a vtkPolyData object to store points and vertices
-polydata = vtk.vtkPolyData()
-polydata.SetPoints(points)
-polydata.SetVerts(verts)
+# Create polydata for neurons
+polydata_neurons = vtk.vtkPolyData()
+polydata_neurons.SetPoints(points)
+polydata_neurons.SetVerts(verts)
 
-# Create a mapper
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputData(polydata)
+# Create mapper for neurons
+mapper_neurons = vtk.vtkPolyDataMapper()
+mapper_neurons.SetInputData(polydata_neurons)
 
-# Create an actor
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
+# Create actor for neurons
+actor_neurons = vtk.vtkActor()
+actor_neurons.SetMapper(mapper_neurons)
+actor_neurons.GetProperty().SetColor(0.0, 0.0, 0.0)  # Set color to black
+actor_neurons.GetProperty().SetPointSize(5.0)  # Set point size to 5.0
 
-# Set actor properties
-actor.GetProperty().SetColor(0.0, 0.0, 0.0)  # Set color to black
-actor.GetProperty().SetPointSize(5.0)  # Set point size to 5.0
+# TODO: ADD FUNCTION THAT DOES CONNECTIVITY PER SIMULATION
+
+# Load network connectivity data
+# Load network data
+fn_network = "/Users/konstantinoskevopoulos/Downloads/network_calcium/rank_0_step_1000000_in_network.txt"
+network_data = np.loadtxt(fn_network, skiprows=4, usecols=(1, 3))
+
+# Create lines for connections
+lines = vtk.vtkCellArray()
+for row in network_data:
+    target_local_id, source_local_id = map(int, row)
+    target_id = local_ids.index(target_local_id)
+    source_id = local_ids.index(source_local_id)
+
+    line = vtk.vtkLine()
+    line.GetPointIds().SetId(0, target_id)
+    line.GetPointIds().SetId(1, source_id)
+    lines.InsertNextCell(line)
+
+# Create polydata for connections
+polydata_connections = vtk.vtkPolyData()
+polydata_connections.SetPoints(points)
+polydata_connections.SetLines(lines)
+
+# Create mapper for connections
+mapper_connections = vtk.vtkPolyDataMapper()
+mapper_connections.SetInputData(polydata_connections)
+
+# Create actor for connections
+actor_connections = vtk.vtkActor()
+actor_connections.SetMapper(mapper_connections)
+actor_connections.GetProperty().SetColor(0.8, 0.8, 0.8)  # Set color to gray
+actor_connections.GetProperty().SetLineWidth(4)  # Set line width
+
+# Create spheres for neurons
+sphere_source = vtk.vtkSphereSource()
+sphere_source.SetRadius(1.4)  # Set the radius of the spheres
+
+glyph = vtk.vtkGlyph3D()
+glyph.SetInputData(polydata_connections)  # Use the connections as input for the nodes
+glyph.SetSourceConnection(sphere_source.GetOutputPort())
+
+# Create mapper for neurons
+mapper_neurons = vtk.vtkPolyDataMapper()
+mapper_neurons.SetInputConnection(glyph.GetOutputPort())
+
+# Create actor for neurons
+actor_neurons = vtk.vtkActor()
+actor_neurons.SetMapper(mapper_neurons)
+actor_neurons.GetProperty().SetColor(1.0, 0.0, 0.0)  # Set color to red
 
 # Create a renderer
 renderer = vtk.vtkRenderer()
@@ -53,8 +104,9 @@ render_window.AddRenderer(renderer)
 render_window_interactor = vtk.vtkRenderWindowInteractor()
 render_window_interactor.SetRenderWindow(render_window)
 
-# Add the actor to the renderer
-renderer.AddActor(actor)
+# Add the actors to the renderer
+renderer.AddActor(actor_connections)
+renderer.AddActor(actor_neurons)
 
 # Set background color
 renderer.SetBackground(1.0, 1.0, 1.0)
